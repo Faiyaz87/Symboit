3e141b12846b2ceab328267f0895341131753433
Faiyaz87 <faiyaz87@gmail.com>
2019-10-08 20:29:41 +0530

    initial add

---
+++ SymbiotWeb/src/main/java/com/symbiot/SymbiotWebApplication.java
@@ -0,0 +1,13 @@
+package com.symbiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbiotWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymbiotWebApplication.class, args);
+	}
+
+}
---
+++ SymbiotWeb/src/main/webapp/WEB-INF/view/login.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="/resources/js/jquery-3.4.1.min.js"></script>
+<meta charset="ISO-8859-1">
+<title>Symbiot</title>
+</head>
+<body>
+
+</body>
+</html>
