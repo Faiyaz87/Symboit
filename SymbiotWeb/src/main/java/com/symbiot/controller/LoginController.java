d87ad23f023e116cb936e39c9eb802c1dc5e9d52
Faiyaz87 <faiyaz87@gmail.com>
2019-10-14 22:51:41 +0530

    login html

--- SymbiotWeb/src/main/java/com/symbiot/controller/LoginController.java
+++ SymbiotWeb/src/main/java/com/symbiot/controller/LoginController.java
@@ -1,15 +1,29 @@
 package com.symbiot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.symbiot.entity.Login;
+import com.symbiot.entity.User;
+import com.symbiot.service.IUserService;
+
 @Controller
 public class LoginController {
 	
+	@Autowired
+	IUserService userService;
+	
 	@RequestMapping("/")
 	public String index(Model model) {
-		model.addAttribute("message","Hi thios if from controller");
+		return "login";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model,Login login) {
+		User userDtls = userService.getUserByLogin(login.getUsername());
+		System.out.println("userDtls : "+userDtls.toString());
 		return "login";
 	}
 
--- SymbiotWeb/src/test/java/com/symbiot/SymbiotWebApplicationTests.java
+++ SymbiotWeb/src/test/java/com/symbiot/SymbiotWebApplicationTests.java
@@ -2,15 +2,23 @@ package com.symbiot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.symbiot.service.IUserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SymbiotWebApplicationTests {
 
+	
+	@Autowired
+	IUserService UserService; 
+	
 	@Test
-	public void contextLoads() {
+	public void test() {
+		System.out.println("User : "+UserService.getUserByLogin("Test"));
 	}
 
 }
